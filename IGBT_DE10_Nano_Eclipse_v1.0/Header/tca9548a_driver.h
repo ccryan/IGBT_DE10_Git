68aa9ad6b880ae4aa7a20a2492e11f3ad4d941d1
Cheng Chen <chenc2442@gmail.com>
2020-05-27 15:36:27 -0400

    FPGA Verilog

--- IGBT_DE10_Nano_Eclipse_v1.0/Header/tca9548a_driver.h
+++
@@ -1,9 +0,0 @@
-#define HUB_IN_USE		0b1
-#define HUB_channel_all_on		0b11111111
-#define HUB_channel_0_on		0b00000001
-#define HUB_channel_1_on		0b00000010
-#define HUB_channel_2_on		0b00000100
-#define HUB_channel_3_on		0b00001000
-#define HUB_channel_4_on		0b00010000
-#define HUB_channel_5_on		0b00100000
-
